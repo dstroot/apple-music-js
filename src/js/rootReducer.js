dc7e5d2b8f6f59f7c2ea3effb4281f1e761bfb67
Dan Stroot <dan.stroot@gmail.com>
2019-02-11 19:54:51 -0800

    working checkpoint

--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,22 +1,35 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { devToolsEnhancer } from 'redux-devtools-extension';
-import viewReducer from './views/reducer';
-import apiReducer from './api/reducer';
-import audioReducer from './audio/reducer';
-import navReducer from './components/bar/reducer';
+import { createStore, compose, combineReducers, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+// import { devToolsEnhancer } from "redux-devtools-extension";
 
+// reducers
+import viewReducer from "./views/reducer";
+import apiReducer from "./api/reducer";
+import audioReducer from "./audio/reducer";
+import navReducer from "./components/bar/reducer";
+
+// combine reducers
 const rootReducer = combineReducers({
-   viewState: viewReducer,
-   apiState: apiReducer,
-   audioState: audioReducer,
-   navState: navReducer,
+  viewState: viewReducer,
+  apiState: apiReducer,
+  audioState: audioReducer,
+  navState: navReducer
 });
 
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     devToolsEnhancer(),
+//     applyMiddleware(thunkMiddleware)
+//   )
+// );
+
 const store = createStore(
-   rootReducer,
-   devToolsEnhancer(),
-   applyMiddleware(thunkMiddleware),
+  rootReducer,
+  compose(
+    applyMiddleware(thunkMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
